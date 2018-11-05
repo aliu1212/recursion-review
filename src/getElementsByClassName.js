02bf31044f4cc90d8ce358eeaff2b9dc70c3d90f
aliu1212 <alexliu12@gmail.com>
2018-11-05 12:43:02 -0800

    in progress

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,28 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
+// You should use document.body, element.children, and element.classList
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+
+  //start with document.body and an empty array
+  //iterate through the classlist, add node if it matches classname
+  //iterate through the children, recurse.
+
+  var results = [];
+  
+  var finder = function(myElement) {
+    for (var i = 0; i < myElement.classList.length; i++) {
+      if (myElement.className === className) {
+        results.push(myElement);
+      }
+    }
+    for (var i = 0; i < myElement.children.length; i++) {
+      finder(myElement.children[i]);
+    }
+  };
+  
+  finder(document.body);
+  return results;
 };
