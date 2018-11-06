88dfea9eb205fc379cef73d2a8a2701deb91143d
aliu1212 <alexliu12@gmail.com>
2018-11-05 17:37:16 -0800

    first stab..

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -5,10 +5,15 @@ describe('parseJSON', function() {
     parseableStrings.forEach(function(test) {
       var result = parseJSON(test);
       var expected = JSON.parse(test);
-      var equality = _.isEqual(result, expected); // why can't we use `===` here?
+      var equality = _.isEqual(result, expected);
+      console.log(result);
+      console.log(expected);
+      expect(equality).to.equal(true);
+      
+      // why can't we use `===` here?
       // Replace this line with an `expect` statement that tests
       // the behavior described by the `it` string
-      throw new Error('Test is missing.')
+      // throw new Error('Test is missing.')
     });
   });
 
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,38 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+
+  //var fn check first char
+  var checkFirst = function(remainder) {
+    if (remainder.charAt(0) === '[') {
+      return addArray(remainder);
+    } 
+    if (remainder.charAt(0) === '{') {
+      return addObject(remainder);
+    } 
+  }
+
+  var addArray = function(remainder) {
+    var temp = [];
+
+    return temp;
+  }
+  
+  var addObject = function(remainder) {
+    var temp = {};
+    if (remainder.charAt(1) === '"') {
+      var key = addString(remainder.slice(2));
+    }
+    
+    temp[key] = "";//value
+    return temp;
+  }
+
+  var addString = function(remainder) {
+    return remainder.substring(0,remainder.indexOf('"'));
+  }  
+
+  return checkFirst(json);
+
+
 };
